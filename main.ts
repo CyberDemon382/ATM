dd52889f643ac188c3aff26fa2e51796a20a1004
Shayan Umer <umershayan91@gmail.com>
2024-03-29 22:05:35 +0500

    A Simple ATM made by the help of TS and Inquirer

---
+++ main.ts
@@ -0,0 +1,40 @@
+import inquirer from "inquirer";
+
+async function main() {
+    const passCode = await inquirer.prompt([{
+        message: "Enter Your Passcode!",
+        type: "number",
+        name: "passCode"
+    }]);
+
+    const myPin = 123456;
+    const myBalance = 10000;
+
+    if (passCode.passCode === myPin) {
+        console.log("Your Balance Is:", myBalance);
+        const choice = await inquirer.prompt([{
+            message: "Choose what you want to do?",
+            name: "choices",
+            type: "list",
+            choices: ["Withdraw Your Money", "See Your Balance"]
+        }]);
+
+        if (choice.choices === "Withdraw Your Money") {
+            const withdrawalAmount = await inquirer.prompt([{
+                message: "Enter Your Amount to Be Withdrawn!",
+                type: "number",
+                name: "amount"
+            }]);
+            let amount = myBalance - withdrawalAmount.amount;
+            console.log("Your Payment Has been Successfully made, and Your remaining Balance is:", amount);
+        } else if (choice.choices === "See Your Balance") {
+            console.log("Your Current Balance Is:", myBalance);
+        } else {
+            console.log("Invalid choice. Please select a valid option.");
+        }
+    } else {
+        console.log("You Have Entered an Invalid Password");
+    }
+}
+
+main();
